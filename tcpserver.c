8940c2c8e7549d33b462365a1adbdb61a79294a5
LouSoto <ls339@njit.edu>
2015-10-19 15:30:30 -0400

    tcpserver

--- tcpserver.c
+++ tcpserver.c
@@ -7,23 +7,48 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+
+void error(char *msg) {
+
+  perror(msg);
+  exit(1);
+}
+
+
 int main(int argc, char **argv) {
 
-  int sock, port;
+  int sockfd, newsockfd, portno, clilen;
   char buffer[256];
-
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
   /*
-   * int socket(int domain, int type, int protocol);
-   * AF_INET : IPv4 Internet protocols
-   * SOCK_STREAM : Provides sequenced, reliable, two-way, connection-based byte streams.  
-   * An out-of-band data transmission mechanism may be supported.
-   * Protocol : The protocol specifies a particular protocol to be used with the socket.  
-   * Normally only a single protocol exists to support a particular socket type within a given 
-   * protocol family, in which case protocol can be specified as 0.
-   */
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-
-  port = 35000;
-  
-  printf("TCP Port : %d\n",port);
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);
+  }
+  */
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    error("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  //portno = atoi(argv[1]);
+  portno = 35000;
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+	   sizeof(serv_addr)) < 0)
+    error("ERROR on binding");
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  if (newsockfd < 0)
+    error("ERROR on accept");
+  bzero(buffer,256);
+  n = read(newsockfd,buffer,255);
+  if (n < 0) error("ERROR reading from socket");
+  printf("Here is the message: %s\n",buffer);
+  n = write(newsockfd,"I got your message",18);
+  if (n < 0) error("ERROR writing to socket");
+  return 0;
 }
