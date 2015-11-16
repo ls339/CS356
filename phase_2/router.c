3ebea24dd4fc551799d4b141bb8fe1e8c42a15a2
ls339 <ls339@njit.edu>
2015-11-15 22:22:53 -0500

    redid design

--- phase_2/router.c
+++ phase_2/router.c
@@ -1,68 +1,140 @@
 /*
  * ls339@njit.edu
- * CS356
+ *
  */
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <sys/types.h>
+#include "routerTable.h"
 
-void error(char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+int main(int argc,char **argv) {
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
+  int sock, newsock, clilen, portno, routerno, n;
+  struct hostent *host;
+  struct sockaddr_in cli_addr;
+  struct sockaddr_in serv_addr;
+  char *route_table, *serial_table;
+  char buffer[1024];
 
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    int routerno, link_cost;
-    char buffer[256];
-    int bufflen;
-    
-    // To-do : represent a table
-    int route[4][4];
-    routerno = 0;
-    link_cost = 0;
-
-    sprintf(buffer,"Router : %d\nLink Cost : %d\n",routerno,link_cost);
-    
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    //printf("Please enter the message: ");
-    //bzero(buffer,256);
-    //fgets(buffer,255,stdin);
-    printf("TCP message received\n");
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+  if(argc < 4) {
+    printf("Usage %s host portno routerno\n",argv[0]);
     return 0;
+  }
+
+  portno = atoi(argv[2]);
+  routerno = atoi(argv[3]);
+
+  printf("host = %s\n",argv[1]);
+  printf("portno = %s\n",argv[2]);
+  printf("routerno = %d\n",routerno);
+
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if(sock < 0) { 
+    printf("Could not create socket\n");
+    return 0;
+  }
+
+  host = gethostbyname(argv[1]);
+  if(host == NULL) {
+    printf("No such host\n");
+  }
+
+  // Initialize our own table
+  struct route_node *tb[N];
+  initRouterTable(tb,routerno);
+
+  // Print out our table
+  route_table = printTable(tb,routerno);
+  printf("%s",route_table);
+
+  // Serialize the table
+  serial_table = serializeTable(tb,routerno);
+  //printf("%s",serial_table);
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+
+  if(routerno!=0) {
+    //bzero((char *) &serv_addr, sizeof(serv_addr));
+    //serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sock, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+      error("ERROR on binding");
+    }
+    listen(sock,5);
+    clilen = sizeof(cli_addr);
+    newsock = accept(sock, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsock < 0) error("ERROR on accept");
+    bzero(buffer,1024);
+    n = read(newsock,buffer,1023);
+    if (n < 0) error("ERROR reading from socket");
+    printf("TCP message received\n");
+    //printf(buffer);
+    printf("Sending router %d table . . .\n",routerno);
+    n = write(newsock,serial_table,strlen(serial_table));
+    if (n < 0) error("ERROR writing to socket");
+  } else {
+    bcopy((char *)host->h_addr,(char *)&serv_addr.sin_addr.s_addr,host->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+      error("ERROR connecting");
+    printf("Sending router %d table . . .\n",routerno);
+    n = write(sock,serial_table,strlen(serial_table));
+    printf("TCP message received\n");
+    n = write(sock,buffer,strlen(buffer));
+    if (n < 0)
+      error("ERROR writing to socket");
+    bzero(buffer,1024);
+    n = read(sock,buffer,1023);
+    if (n < 0)
+      error("ERROR reading from socket");
+    //printf("%s\n",buffer);
+  }
+  
+  close(sock);
+  close(newsock);
+
+  printf("%s\n",buffer);
+  //printf("length = %d\n",strlen(buffer));
+  int i,j;
+  int remote_router = buffer[0] - '0';
+  struct route_node *remote_tb[N];
+  for(i=0;i<N;i++) {
+    remote_tb[i] = malloc(sizeof(struct route_node));
+  }
+  printf("remote router = %d\n",remote_router);
+  
+  i=1;
+  j=0;
+  while(buffer[i] != '\0') {
+    //printf("remote_tb[%d]->dest = %c\n",j,buffer[i]);
+    remote_tb[j]->dest = buffer[i] - '0';
+    i++;
+    //printf("remote_tb[%d]->interface = %c\n",j,buffer[i]);
+    remote_tb[j]->interface = buffer[i];
+    i++;
+    //printf("remote_tb[%d]->cost = %c\n",j,buffer[i]);
+    remote_tb[j]->cost = buffer[i] - '0';
+    i++;
+    j++;
+  }
+  printf("%s",printTable2(remote_tb,remote_router));
+  //int j = strlen(buffer);
+  //for (i=0;i<N;i++) {
+  //printf("length - 1 = %d\n",(j-1));
+    //remote_tb[i-1]->dest = buffer[i] - '0';
+    //remote_tb[i-1]->interface = buffer[i+1] - '0';
+    //remote_tb[i-1]->cost = buffer[i+2] - '0';
+  //}
+  
+  //  while(buffer[i] != '\0') {
+  //   printf("%c",buffer[i]);
+    //router_tb[i]->dest = 
+  //   i++;
+  // }
+
+  return 0;
 }
---
+++ phase_2/routerTable.h
@@ -0,0 +1,144 @@
+/*
+ * ls339@njit.edu
+ * Header file to create and manipulate router tables.
+ */
+
+#include <stdlib.h>	
+#include <string.h>
+
+#define N 4
+
+struct route_node{
+  int dest;
+  char interface;
+  int cost;
+};
+
+void initRouterTable(struct route_node **tb, int routerno) {
+  int i;
+  for(i=0;i<N;i++) {
+    tb[i] = malloc(sizeof(struct route_node));
+  }
+  if(routerno==0){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = 'l';
+    tb[0]->cost = 0;
+
+    tb[1]->dest = 1;
+    tb[1]->interface = '0';
+    tb[1]->cost = 1;
+
+    tb[2]->dest = 2;
+    tb[2]->interface = '1';
+    tb[2]->cost = 3;
+
+    tb[3]->dest = 3;
+    tb[3]->interface = '2';
+    tb[3]->cost = 7;
+  }
+  if(routerno==1){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = '2';
+    tb[0]->cost = 1;
+
+    tb[1]->dest = 1;
+    tb[1]->interface = 'l';
+    tb[1]->cost = 0;
+
+    tb[2]->dest = 2;
+    tb[2]->interface = '0';
+    tb[2]->cost = 1;
+
+    // No direct link : set to -1
+    tb[3]->dest = -1;
+    tb[3]->interface = 'E';
+    tb[3]->cost = -1;
+  }
+  if(routerno==2){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = '2';
+    tb[0]->cost = 3;
+
+    tb[1]->dest = 1;
+    tb[1]->interface = '0';
+    tb[1]->cost = 1;
+
+    tb[2]->dest = 2;
+    tb[2]->interface = 'l';
+    tb[2]->cost = 0;
+
+    tb[3]->dest = 3;
+    tb[3]->interface = '1';
+    tb[3]->cost = 2;
+  }
+  if(routerno==3){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = '0';
+    tb[0]->cost = 7;
+
+    // No direct link : set to -1
+    tb[1]->dest = -1;
+    tb[1]->interface = 'E';
+    tb[1]->cost = -1;
+    
+
+    tb[2]->dest = 2;
+    tb[2]->interface = '2';
+    tb[2]->cost = 2;
+
+    tb[3]->dest = 3;
+    tb[3]->interface = 'l';
+    tb[3]->cost = 0;
+  }
+}
+
+void updateCost(struct route_node **tb, int dest, int cost) {
+  if(tb[dest]->interface!='l') tb[dest]->cost = cost;
+}
+
+char * printTable(struct route_node **tb,int routerno) {
+  char *str = (char *) malloc(1024);
+  int i;
+  sprintf(str,"+--------------------------------------------+\n");
+  sprintf(str + strlen(str),"| Router %d                                   |\n",routerno);
+  sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  sprintf(str + strlen(str),"| Destination Router | Interface | Link Cost |\n");
+  sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  for(i=0;i<N;i++){
+    if(tb[i]->dest==-1) continue;
+    sprintf(str + strlen(str),"| %18d | %9c | %9d |\n",tb[i]->dest,tb[i]->interface,tb[i]->cost);
+    sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  }
+  return str;
+}
+
+char * printTable2(struct route_node **tb,int routerno) {
+  char *str = (char *) malloc(1024);
+  int i;
+  sprintf(str,"+--------------------------------------------+\n");
+  sprintf(str + strlen(str),"| Router %d                                   |\n",routerno);
+  sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  sprintf(str + strlen(str),"| Destination Router | Interface | Link Cost |\n");
+  sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  for(i=0;i<N;i++){
+    if(tb[i]->dest==-1) continue;
+    sprintf(str + strlen(str),"| %18d | %9c | %9d |\n",tb[i]->dest,tb[i]->interface,tb[i]->cost);
+    sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  }
+  return str;
+}
+
+char * serializeTable(struct route_node **tb, int routerno) {
+  char *str = (char *) malloc(1024);
+  int i;
+  sprintf(str,"%d",routerno);
+  for(i=0;i<N;i++){
+    if(tb[i]->dest==-1) continue;
+    sprintf(str + strlen(str),"%d%c%d",tb[i]->dest, tb[i]->interface,tb[i]->cost);
+  } 
+  return str;
+}
