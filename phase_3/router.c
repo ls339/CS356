79a08bd6134a598b10a1dbf11c336450260137e3
ls339 <ls339@njit.edu>
2015-11-28 20:08:11 -0500

    phase 3

---
+++ phase_3/router.c
@@ -0,0 +1,156 @@
+/*
+ *
+ * ls339@njit.edu
+ *
+ */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include "routerTable.h"
+
+int main(int argc,char **argv) {
+
+  int sock, newsock, rsock, clilen, portno, routerno, n;
+  int i,j, num_dc_hosts, remote_router;
+  struct hostent *host, *rhost;
+  struct sockaddr_in cli_addr;
+  struct sockaddr_in serv_addr,remote_addr;
+  char buffer[1024];
+  char *dc_hosts[N];
+  char *serial_table;
+  struct route_node *tb[N];
+  struct route_node *remote_tb[N];
+
+  if(argc < 4) {
+    printf("Usage %s host portno routerno\n",argv[0]);
+    return 0;
+  }
+
+  portno = atoi(argv[2]);
+  routerno = atoi(argv[3]);
+  //initRouterTable(tb,routerno);
+  
+  if(routerno==0) {
+    num_dc_hosts = 3;
+    dc_hosts[0] = "localhost";
+    dc_hosts[1] = "127.0.0.1";
+    dc_hosts[3] = "127.0.0.1";
+  } else if(routerno==1) {
+    num_dc_hosts = 2;
+    dc_hosts[0] = "192.168.1.6";
+    dc_hosts[1] = "192.168.1.3";
+  } else if(routerno==2) {
+    num_dc_hosts = 3;
+    dc_hosts[0] = "localhost";
+    dc_hosts[1] = "127.0.0.1";
+    dc_hosts[3] = "127.0.0.1";
+  }  if(routerno==3) {
+    num_dc_hosts = 2;
+    dc_hosts[0] = "localhost";
+    dc_hosts[1] = "127.0.0.1";
+  }
+
+  enum state status;
+  // printf("host = %s, port = %s\n",argv[1],argv[2]);
+  initRouterTable(tb,routerno);
+  serial_table = serializeTable(tb,routerno);
+  status = INIT;
+  
+  sock = socket(AF_INET, SOCK_STREAM, 0); // Handle error
+  //rsock = socket(AF_INET, SOCK_STREAM, 0);
+  host = gethostbyname(argv[1]); // Handle error
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  bind(sock, (struct sockaddr *) &serv_addr,sizeof(serv_addr)); // handle error
+  
+  remote_addr.sin_family = AF_INET;
+  remote_addr.sin_port = htons(portno);
+  //listen(sock,5);
+  //clilen = sizeof(cli_addr);
+  //newsock = accept(sock, (struct sockaddr *) &cli_addr, &clilen);
+  //bzero(buffer,1024);
+  //n = read(newsock,buffer,1023);
+
+  //c=getchar();
+  //printf("out of loop\n");
+  //listen(sock,5);
+  //status = NOTUPDATED;
+
+  printf("%s",printTable(tb,routerno));
+  printf("serial table = %s\n",serial_table);
+  while(1) {
+    if(routerno==1 && status==INIT) {
+      printf("Sending out intial table\n");
+      for(i=0;i<num_dc_hosts;i++) {
+	printf("--> %s\n",dc_hosts[i]);
+	rsock = socket(AF_INET, SOCK_STREAM, 0);
+	rhost = gethostbyname(dc_hosts[i]);
+	bcopy((char *)rhost->h_addr,(char *)&remote_addr.sin_addr.s_addr,rhost->h_length);
+	//remote_addr.sin_port = htons(portno);
+	//connect(rsock,(struct sockaddr *)&remote_addr,sizeof(remote_addr));
+	//printf("xxx\n");
+	if (connect(rsock,(struct sockaddr *) &remote_addr,sizeof(remote_addr)) < 0) {
+	  printf("ERROR connecting\n");
+	}
+	//printf("yyy\n");
+	n = write(rsock,serial_table,strlen(serial_table));
+	//printf("%s",printTable(tb,routerno));
+	printf("eol\n");
+	close(rsock);
+      }
+    }
+      //bcopy((char *)host->h_addr,(char *)&remote_addr.sin_addr.s_addr,host->h_length);
+      //remote_addr.sin_port = htons(portno);
+      //connect(sock,(struct sockaddr *)&remote_addr,sizeof(remote_addr));
+      //n = write(sock,serial_table,strlen(serial_table));
+    listen(sock,5);
+    clilen = sizeof(cli_addr);
+    newsock = accept(sock, (struct sockaddr *) &cli_addr, &clilen);
+    bzero(buffer,1024);
+    n = read(newsock,buffer,1023);
+    printf("in loop\n");
+    //remote_router = buffer[0] - '0';
+    //initRouterTable(remote_tb,-1);
+
+    /*
+     * Take received serialized data
+     * and convert it to route_node struct.
+     */
+    remote_router = buffer[0] - '0';
+    initRouterTable(remote_tb,-1);
+    i=1;
+    j=0;
+    while(buffer[i] != '\0') {
+      remote_tb[j]->dest = buffer[i] - '0';
+      i++;
+      remote_tb[j]->interface = buffer[i];
+      i++;
+      remote_tb[j]->cost = buffer[i] - '0';
+      i++;
+      //printf("%s",printTable(tb,i));
+      j++;
+    }
+    
+    //printf("remote router = %d\n",remote_router);
+    //if( (BellmanFord2(tb,remote_tb,remote_router)) == 1) {
+    //	status = UPDATED;
+    //  } else {
+    //	status = NOTUPDATED;
+    //  }
+    //printf("%s",buffer);
+    //printf("%s",printTable(tb,routerno));
+    status = BellmanFord2(tb,remote_tb,remote_router);
+    printf("status = %d \n",status);
+    if(status == UPDATED) {
+      printf("%s",printTable(tb,routerno));
+    }
+  }
+  
+  
+  return 0;
+}
---
+++ phase_3/routerTable.h
@@ -0,0 +1,185 @@
+/*
+ * ls339@njit.edu
+ * Header file to create and manipulate router tables.
+ */
+
+#include <stdlib.h>	
+#include <string.h>
+
+#define N 4
+#define INFINITY 256
+
+struct route_node{
+  int dest;
+  char interface;
+  int cost;
+};
+
+typedef enum state{
+  INIT,
+  UPDATED,
+  NOTUPDATED
+} state;
+
+void initRouterTable(struct route_node **tb, int routerno) {
+  int i;
+  for(i=0;i<N;i++) {
+    tb[i] = malloc(sizeof(struct route_node));
+  }
+  if(routerno==0){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = 'l';
+    tb[0]->cost = 0;
+
+    tb[1]->dest = 1;
+    tb[1]->interface = '0';
+    tb[1]->cost = 1;
+
+    tb[2]->dest = 2;
+    tb[2]->interface = '1';
+    tb[2]->cost = 3;
+
+    tb[3]->dest = 3;
+    tb[3]->interface = '2';
+    tb[3]->cost = 7;
+  }
+  if(routerno==1){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = '2';
+    tb[0]->cost = 1;
+
+    tb[1]->dest = 1;
+    tb[1]->interface = 'l';
+    tb[1]->cost = 0;
+
+    tb[2]->dest = 2;
+    tb[2]->interface = '0';
+    tb[2]->cost = 1;
+
+    // No direct link : set to INFINITY
+    tb[3]->dest = 3;
+    tb[3]->interface = 'X';
+    tb[3]->cost = INFINITY;
+  }
+  if(routerno==2){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = '2';
+    tb[0]->cost = 3;
+
+    tb[1]->dest = 1;
+    tb[1]->interface = '0';
+    tb[1]->cost = 1;
+
+    tb[2]->dest = 2;
+    tb[2]->interface = 'l';
+    tb[2]->cost = 0;
+
+    tb[3]->dest = 3;
+    tb[3]->interface = '1';
+    tb[3]->cost = 2;
+  }
+  if(routerno==3){
+
+    tb[0]->dest = 0;
+    tb[0]->interface = '0';
+    tb[0]->cost = 7;
+
+    // No direct link : set to INFINITY
+    tb[1]->dest = 1;
+    tb[1]->interface = 'X';
+    tb[1]->cost = INFINITY;
+    
+
+    tb[2]->dest = 2;
+    tb[2]->interface = '2';
+    tb[2]->cost = 2;
+
+    tb[3]->dest = 3;
+    tb[3]->interface = 'l';
+    tb[3]->cost = 0;
+  }
+  if(routerno==-1){
+    //Intialize remote router
+    for(i=0;i<N;i++){
+      tb[i]->dest = i;
+      tb[i]->interface = 'X';
+      tb[i]->cost = INFINITY;
+    }
+  }
+}
+
+void updateRoute(struct route_node **tb, int dest, int cost, char iface) {
+  if(tb[dest]->interface!='l') { 
+    tb[dest]->cost = cost;
+    tb[dest]->interface = iface;
+  }
+}
+
+char * printTable(struct route_node **tb,int routerno) {
+  char *str = (char *) malloc(1024);
+  int i;
+  sprintf(str,"+--------------------------------------------+\n");
+  sprintf(str + strlen(str),"| Router %d                                   |\n",routerno);
+  sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  sprintf(str + strlen(str),"| Destination Router | Interface | Link Cost |\n");
+  sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  for(i=0;i<N;i++){
+    if(tb[i]->cost==INFINITY) {
+      sprintf(str + strlen(str),"| %18d | %9c |  Infinity |\n",tb[i]->dest,tb[i]->interface);
+    } else {
+      sprintf(str + strlen(str),"| %18d | %9c | %9d |\n",tb[i]->dest,tb[i]->interface,tb[i]->cost);
+    }
+    sprintf(str + strlen(str),"+--------------------------------------------+\n");
+  }
+  return str;
+}
+
+char * serializeTable(struct route_node **tb, int routerno) {
+  char *str = (char *) malloc(1024);
+  int i;
+  sprintf(str,"%d",routerno);
+  for(i=0;i<N;i++){
+    if(tb[i]->cost==INFINITY) continue;
+    sprintf(str + strlen(str),"%d%c%d",tb[i]->dest, tb[i]->interface,tb[i]->cost);
+  } 
+  return str;
+}
+
+void BellmanFord(struct route_node **local, struct route_node **remote, int remotert){
+  int i;
+  for(i=0;i<N;i++){
+    if(local[i]->interface=='l'||remote[i]->interface=='l') continue;
+    int newCost = remote[i]->cost+local[remotert]->cost;
+    if(local[i]->cost > newCost) {
+      printf("Updating : dest = %d to cost = %d via iface = %c\n",local[i]->dest,newCost,local[remotert]->interface);
+      updateRoute(local,local[i]->dest,newCost,local[remotert]->interface);
+    }
+  }
+}
+
+state BellmanFord2(struct route_node **local, struct route_node **remote, int remotert){
+  int i;
+  //int newCost;
+  enum state status = NOTUPDATED;
+  //enum state status = NOTUPDATED;
+  //printf("%s",printTable(local,remotert));
+  for(i=0;i<N;i++){
+    if(local[i]->interface=='l'||remote[i]->interface=='l') continue;
+    //printf("remotert = %d\n",remotert);
+    //printf("remote[%d]->cost = %d\n",i,remote[i]->cost);
+    //printf("local[%d]->cost = %d\n",remotert,local[remotert]->cost);
+    int newCost = remote[i]->cost+local[remotert]->cost;
+    //printf("new cost = %d\n",newCost);
+    //printf("local cost = %d\n",local[i]->cost);
+    if(local[i]->cost > newCost) {
+      printf("Updating : dest = %d to cost = %d via iface = %c\n",local[i]->dest,newCost,local[remotert]->interface);
+      //printf("updating local 
+      updateRoute(local,local[i]->dest,newCost,local[remotert]->interface);
+      status = UPDATED;
+    }
+  }
+  return status;
+}
